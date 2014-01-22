66e9b5a5b597a87bb24a12e7026cb7d6f5f360c9
Abimael Martell <abimex@gmail.com>
2014-01-21 16:19:59 -0800

    add post layout, change header background attachment

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+/// analitycs and comments
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-36375037-1', 'abimaelmartell.com');
+ga('send', 'pageview');
+
+var disqus_shortname = 'abimaelmartell-blog';
+
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.type = 'text/javascript';
+  s.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());

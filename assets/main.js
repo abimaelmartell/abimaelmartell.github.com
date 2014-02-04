7dfa4623ee783ac84763cddf0b544ef20e952eb4
Abimael Martell <abimex@gmail.com>
2014-02-03 23:07:01 -0800

    add rss feed and organize main.js

--- assets/main.js
+++ assets/main.js
@@ -3,18 +3,15 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-39747273-1']);
 _gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
 var disqus_shortname = 'abimaelmartell-blog';
 
-(function () {
-  var s = document.createElement('script'); s.async = true;
-  s.type = 'text/javascript';
-  s.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
+var require = function(source){
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = source;
+  document.getElementsByTagName('body')[0].appendChild(script)
+}
+
+require('//google-analytics.com/ga.js');
+require('//' + disqus_shortname + '.disqus.com/embed.js');
